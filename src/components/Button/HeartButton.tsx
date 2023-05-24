284119eb074d0127cab9564db244a932a80706e8
cocozzang <cocorlawnghks@gmail.com>
2023-05-25 00:30:20 +0900

    feat: add listing favoritting
    favorite POST DELETE api 추가, add useFavorite hook to access favorite api
    
    currenUser값의 props drilling문제는 react-query를 활용해보자

--- src/components/Button/HeartButton.tsx
+++ src/components/Button/HeartButton.tsx
@@ -3,6 +3,7 @@
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
 
 import { SafeUser } from '@/types'
+import useFavorite from '@/hooks/useFavorite'
 
 interface HeartButtonProps {
   listingId: string
@@ -13,8 +14,10 @@ export default function HeartButton({
   listingId,
   currentUser,
 }: HeartButtonProps) {
-  const hasFavorited = false
-  const toggleFavorite = () => {}
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  })
 
   return (
     <div
